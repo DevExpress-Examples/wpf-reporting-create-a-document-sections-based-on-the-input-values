b7f872bf02d06c9f7f546e66fcf784c598bb1da1
DevExpressExampleBot <fakeEmail>
2024-01-30 15:04:35 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,62 +1,49 @@
-Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.Printing
 
 Namespace PrintDocumentFromUIWpf
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits DXWindow
+
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
-        Sub DXWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Private Sub DXWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
             InitDefaultVlaues()
         End Sub
 
-        Sub btnPrint_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Private Sub btnPrint_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             CreateAndPreviewLink()
         End Sub
 
-        Sub InitDefaultVlaues()
-            teReportHeader.EditValue = "Report Header"
-            tePageHeader.EditValue = "Page Header"
-            teDetail.EditValue = "Detail"
-            tePageFooter.EditValue = "Page Footer"
-            teReportFooter.EditValue = "Report Footer"
-            seDetail.EditValue = 400
-            cePageInfo.EditValue = True
+        Private Sub InitDefaultVlaues()
+            Me.teReportHeader.EditValue = "Report Header"
+            Me.tePageHeader.EditValue = "Page Header"
+            Me.teDetail.EditValue = "Detail"
+            Me.tePageFooter.EditValue = "Page Footer"
+            Me.teReportFooter.EditValue = "Report Footer"
+            Me.seDetail.EditValue = 400
+            Me.cePageInfo.EditValue = True
         End Sub
 
-        Sub CreateAndPreviewLink()
-            Dim link As New SimpleLink With {
-                .ReportHeaderTemplate = CType(Resources("reportHeaderTemplate"), DataTemplate),
-                .PageHeaderTemplate = CType(Resources("pageHeaderTemplate"), DataTemplate),
-                .DetailTemplate = CType(Resources("detailTemplate"), DataTemplate),
-                .PageFooterTemplate = CType(Resources("pageFooterTemplate"), DataTemplate),
-                .ReportFooterTemplate = CType(Resources("reportFooterTemplate"), DataTemplate),
-                .ReportHeaderData = teReportHeader,
-                .PageHeaderData = tePageHeader,
-                .PageFooterData = New PageFooterDataContext With {
-                    .Title = tePageFooter.EditValue.ToString(),
-                    .PrintPageInfo = CBool(cePageInfo.IsChecked)
-                },
-                .ReportFooterData = teReportFooter,
-                .DetailCount = CInt(seDetail.EditValue)
-            }
-
+        Private Sub CreateAndPreviewLink()
+            Dim link = New SimpleLink With {.ReportHeaderTemplate = CType(Resources("reportHeaderTemplate"), DataTemplate), .PageHeaderTemplate = CType(Resources("pageHeaderTemplate"), DataTemplate), .DetailTemplate = CType(Resources("detailTemplate"), DataTemplate), .PageFooterTemplate = CType(Resources("pageFooterTemplate"), DataTemplate), .ReportFooterTemplate = CType(Resources("reportFooterTemplate"), DataTemplate), .ReportHeaderData = Me.teReportHeader, .PageHeaderData = Me.tePageHeader, .PageFooterData = New PageFooterDataContext With {.Title = Me.tePageFooter.EditValue.ToString(), .PrintPageInfo = CBool(Me.cePageInfo.IsChecked)}, .ReportFooterData = Me.teReportFooter, .DetailCount = CInt(Me.seDetail.EditValue)}
             AddHandler link.CreateDetail, AddressOf link_CreateDetail
-
             link.ShowPrintPreviewDialog(Me)
         End Sub
 
-        Sub link_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
-            e.Data = String.Format("{0} (Row {1})", teDetail.EditValue, e.DetailIndex)
+        Private Sub link_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+            e.Data = String.Format("{0} (Row {1})", Me.teDetail.EditValue, e.DetailIndex)
         End Sub
     End Class
 
     Public Class PageFooterDataContext
-        Public Property Title() As String
-        Public Property PrintPageInfo() As Boolean
+
+        Public Property Title As String
+
+        Public Property PrintPageInfo As Boolean
     End Class
 End Namespace
